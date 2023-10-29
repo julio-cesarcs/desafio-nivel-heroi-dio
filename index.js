e15f6c3b2468197a927c9669d5defc8582268e60
jcoii39 <julio_cezzar@yahho.com.br>
2023-10-28 22:19:00 -0300

    desafio do curso de programação da DIO

---
+++ index.js
@@ -0,0 +1,23 @@
+const nomeHeroi = "Weregarurumon"
+let xpHeroi = 3999
+let nivelHeroi
+
+if (xpHeroi <= 1000) {
+    nivelHeroi = 'Ferro'
+} else if (xpHeroi > 1000 && xpHeroi <= 2000) {
+    nivelHeroi = 'Bronze'
+} else if (xpHeroi > 2000 && xpHeroi <= 5000) {
+    nivelHeroi = 'Prata'
+} else if (xpHeroi > 5000 && xpHeroi <= 7000) {
+    nivelHeroi = 'Ouro'
+} else if (xpHeroi > 7000 && xpHeroi <= 8000) {
+    nivelHeroi = 'Platina'
+} else if (xpHeroi > 8000 && xpHeroi <= 9000) {
+    nivelHeroi = 'Ascendente'
+} else if (xpHeroi > 9000 && xpHeroi <= 10000) {
+    nivelHeroi = 'Imortal'
+} else if (xpHeroi > 10000) {
+    nivelHeroi = 'Radiante'
+}
+
+console.log(`O nível do herói ${nomeHeroi} é ${nivelHeroi}`)
